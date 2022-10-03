e6dac83c4ef58e3634a03048562ebceafefa3337
Felix Chan <felixc63@nycstudents.net>
2022-10-03 12:27:09 -0400

    Finished

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int xANDy){
+        this.x = xANDy;
+        this.y = xANDy;
+    }
+
+    public Point(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int newX){
+        x = newX;
+    }
+
+    public void setY(int newY){
+        y = newY;
+    }
+
+    public String coordinate(){
+        return "(" + x + ", " + y + ")";
+    }
+
+    public String quadrant(){
+        String quadrant;
+        if (x>0 && y>0){
+            quadrant =  "I";
+        }
+        else if (x<0 && y>0){
+            quadrant = "II";
+        }
+        else if (x<0 && y<0){
+            quadrant = "III";
+        }
+        else if (x>0 && y<0){
+            quadrant = "IV";
+        }
+        else if (x==0 && y==0){
+            quadrant = "origin";
+        }
+        else {
+            quadrant = "on axis";
+        }
+
+        return quadrant;
+    }
 }
